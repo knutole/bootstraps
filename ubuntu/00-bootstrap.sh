86e3d435e70a5f9116212c7e1ff1f629a16d50a3
knutole <knutole@mapic.io>
2021-03-06 16:11:08 +0100

    simplify

--- ubuntu/00-bootstrap.sh
+++ ubuntu/00-bootstrap.sh
@@ -6,15 +6,6 @@ echo "NODE: $NODE"
 echo "HOME: $HOME"
 echo "K8_NODE_TYPE: $K8_NODE_TYPE"
 
-# get deps
-sudo apt-get update && sudo apt-get install -y curl unzip
-
-# pull script
-mkdir ~/.bootstraps && cd ~/.bootstraps
-curl -L https://github.com/knutole/bootstraps/archive/main.zip -o bootstraps.zip
-unzip bootstraps.zip
-cd bootstraps-main/ubuntu
-
 # run scripts in sorted order
 for CURR_FILE in $(ls -I 00* -1v *.ubuntu.*) # ignore 00-boostrap, sort, show only ubuntu 
 do
