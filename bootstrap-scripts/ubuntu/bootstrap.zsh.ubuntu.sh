fa590c8388df699772b8c6bc2eccdcbd861bbfc2
knutole <knutole@mapic.io>
2021-03-06 02:02:04 +0000

    update zsh bs for ubuntu

---
+++ bootstrap-scripts/ubuntu/bootstrap.zsh.ubuntu.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#
+# install zsh + powerlevel10k + plugins 
+# os: ubuntu
+# v. 21.03.06 
+# 
+# √ tested
+# 
+
+# install zsh
+sudo apt-get update && sudo apt-get -y install zsh git wget
+
+# install oh-my-zsh
+curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh --output ~/install-oh-my-zsh.sh
+sh ~/install-oh-my-zsh.sh --unattended
+rm ~/install-oh-my-zsh.sh 
+
+# install powerlevel10k theme in oh-my-zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
+
+# install zsh plugins
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone --depth=1 https://github.com/johanhaleby/kubetail.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/kubetail
+
+# get & overwrite zsh config
+curl https://raw.githubusercontent.com/knutole/bootstraps/main/.zshrc -H "Cache-Control: no-cache"  -L > ~/.zshrc
+
+# get powerlevel10k config
+curl https://raw.githubusercontent.com/knutole/bootstraps/main/.p10k.zsh -H "Cache-Control: no-cache"  -L > ~/.p10k.zsh
+
+# set as default
+sudo chsh -s /bin/zsh ubuntu
+
+# activate
+exec zsh
